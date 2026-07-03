57235c9f900b647d09170854d89cbcbf5f239cf1
Raúl Fernández <146001066+rraul10@users.noreply.github.com>
2026-07-03 20:17:26 +0200

    Actualizacion 03/07/2026

--- src/app/components/vaper-list-public/vaper-list-public.component.ts
+++ src/app/components/vaper-list-public/vaper-list-public.component.ts
@@ -481,7 +481,7 @@ export class VaperListPublicComponent implements OnInit {
         nombre: '120K (6 en 1)',
         precio: 14,
         precioEur: 14,
-        stock: 6,
+        stock: 5,
         tipo: '120k',
         color: 'linear-gradient(135deg, #ec4899, #8b5cf6)',
         emoji: '🫐',
